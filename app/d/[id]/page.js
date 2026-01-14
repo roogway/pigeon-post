906d706fbf5ed16c086f8ce34275c633a5b1ebaf
roogway <123575988+roogway@users.noreply.github.com>
2026-01-14 21:10:08 +0530

    Update page.js

--- app/d/[id]/page.js
+++ app/d/[id]/page.js
@@ -56,6 +56,7 @@ export default async function DeliveryPage({ params }) {
       name: data.item_name,
     },
     recipientName: data.recipient_name,
+    senderName: data.sender_name,
     note: data.note,
   }
 
